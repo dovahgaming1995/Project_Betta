68f0d4f280c26a897de775b0aaa37c63d7c18b61
ao20o02 <markazoussayli@gmail.com>
2023-12-05 10:55:48 +0200

    C++ Code

---
+++ Source/GASCompanionDemo/Private/Core/Character/AI/AICharacterBase.cpp
@@ -0,0 +1,56 @@
+﻿// Copyright 2021-2022 Mickael Daniel. All Rights Reserved.
+
+
+#include "Core/Character/AI/AICharacterBase.h"
+
+#include "Abilities/Attributes/GSCAttributeSet.h"
+#include "Attributes/XPAttributeSet.h"
+
+
+// Sets default values
+AAICharacterBase::AAICharacterBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAICharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	if(!GSCAbilitySystemComponent)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Connected"))
+	}
+
+}
+
+// Called every frame
+void AAICharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+void AAICharacterBase::OnHealthChange(const FOnAttributeChangeData& Data)const
+{
+	UE_LOG(LogTemp, Error, TEXT("Killed"));
+}
+
+void AAICharacterBase::InitAb( UGSCAbilitySystemComponent* GSCAbilitySystem)
+{
+	if (GSCAbilitySystem)
+	{
+		GSCAbilitySystemComponent=GSCAbilitySystem;
+		if(const UGSCAttributeSet* Att =Cast<UGSCAttributeSet>(GSCAbilitySystemComponent->GetAttributeSet(UAttributeSet::StaticClass())))
+		{
+			UE_LOG(LogTemp, Error, TEXT("Done"));
+			GSCAbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Att->GetHealthAttribute()).AddUObject(this,&AAICharacterBase::OnHealthChange);
+		}
+	}
+}
+
+// Called to bind functionality to input
+void AAICharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/GASCompanionDemo/Public/Core/GameState/GS_TopDownShooter.h
@@ -0,0 +1,33 @@
+﻿// Copyright 2021-2022 Mickael Daniel. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "GS_TopDownShooter.generated.h"
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FhandleAddExpSignature);
+/**
+ * 
+ */
+UCLASS()
+
+class GASCOMPANIONDEMO_API  AGS_TopDownShooter : public AGameState
+{
+	GENERATED_BODY()
+public:
+	// Override Default Func
+	AGS_TopDownShooter();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	// EXP
+	UPROPERTY(Replicated,BlueprintReadOnly,Category="EXP")
+	float Exp;
+	UPROPERTY(EditDefaultsOnly,Replicated,BlueprintReadOnly,Category="EXP")
+	float EXPRemaining;
+	UFUNCTION(BlueprintCallable)
+	void addExp(float value);
+	UPROPERTY(BlueprintAssignable,Category="EXP")
+	FhandleAddExpSignature handleAddExp;
+
+
+};
